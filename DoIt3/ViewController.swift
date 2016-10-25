461b809beb0f7e2c145cc212c13981847ad60471
marcstreeter <mstreeter@gmail.com>
2016-10-25 12:46:10 -0500

    Revert "added a back button added a + button and added addSegue"
    This reverts commit a504ba33ecf577a6b6a88fc6d1cf4a9a34c83152.

--- DoIt3/ViewController.swift
+++ DoIt3/ViewController.swift
@@ -59,10 +59,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     }
         
-    @IBAction func plusTapped(_ sender: AnyObject) {
-    performSegue(withIdentifier: "addSegue", sender: nil)
-    
-    }
     
     
     
